39bf604a270869362141c5dce7afaa21f29c90a4
wzp-12377 <2918684141@qq.com>
2026-03-25 21:43:33 +0800

    Restore SYN6658 TTS prompts to task1

--- App/task1.c
+++ App/task1.c
@@ -1,10 +1,10 @@
-
-////#include "../MY/main.h"
+#include "main.h"
 #include "state_machine.h"
 #include "pid.h"
 #include "motor.h"
 #include "mpu6050.h"
 #include "infrared.h"
+#include "syn6658.h"
 
 // 脉冲数/100cm 比例。用户说100cm测出来2591个脉冲。
 #define PULSES_PER_100CM  2591.0f
@@ -29,15 +29,36 @@ static float get_angle_diff(float angle1, float angle2) {
     return diff;
 }
 
+void Blink_LED_PC13(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    // 初始化PC13为输出
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+    
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13); // 亮灯
+    delay_ms(200);
+    GPIO_SetBits(GPIOC, GPIO_Pin_13);   // 灭灯
+}
+
 void Task1_Run(void)
 {
     s_move_stage = M_STAGE_INIT;
     SM_Init(&g_state_machine, 3); // 任意
-    
+
     Motor_Enable(1);
     IR_ResetTracking();
     s_start_yaw = g_mpu_data.yaw;
 
+    // 起点A播报: "起点A点"
+    SYN6658_Speak("\xC6\xF0\xB5\xE3\x41\xB5\xE3"); 
+    Blink_LED_PC13();
+    
     s_move_stage = M_STAGE_A_TO_B;
 
     while (1)
@@ -50,18 +71,24 @@ void Task1_Run(void)
             float diff = get_angle_diff(g_mpu_data.yaw, s_start_yaw);
             if (diff > 170.0f || diff < -170.0f) {
                 // 转过了 180度
+                // 播报: "到达C点"
+                SYN6658_Speak("\xB5\xBD\xB4\xEF\x43\xB5\xE3"); 
+                Blink_LED_PC13();
                 s_move_stage = M_STAGE_C_TO_D;
                 // 让它稍微再走一点点避免立即退出黑线
             }
-        } 
+        }
         else if (s_move_stage == M_STAGE_D_TO_A) {
             float diff = get_angle_diff(g_mpu_data.yaw, s_start_yaw);
             if (diff > 170.0f || diff < -170.0f) {
                 // 转过了 180度
+                // 播报: "回到A点"
+                SYN6658_Speak("\xBB\xD8\xB5\xBD\x41\xB5\xE3"); 
+                Blink_LED_PC13();
                 s_move_stage = M_STAGE_DONE;
             }
         }
-        else 
+        else
         {
             // 在直线段，检查路口黑线
             CarEvent_TypeDef evt = EVENT_NONE;
@@ -71,14 +98,20 @@ void Task1_Run(void)
                     evt = EVENT_CROSSROAD;
                 }
             } else {
-                g_ir_data.tracking_mode = 1; 
+                g_ir_data.tracking_mode = 1;
             }
 
             if (evt == EVENT_CROSSROAD) {
                 if (s_move_stage == M_STAGE_A_TO_B) {
+                    // 播报: "到达B点"
+                    SYN6658_Speak("\xB5\xBD\xB4\xEF\x42\xB5\xE3"); 
+                    Blink_LED_PC13();
                     s_move_stage = M_STAGE_B_TO_C;
                     s_start_yaw = g_mpu_data.yaw; // 记录开始转弯的角度
                 } else if (s_move_stage == M_STAGE_C_TO_D) {
+                    // 播报: "到达D点"
+                    SYN6658_Speak("\xB5\xBD\xB4\xEF\x44\xB5\xE3"); 
+                    Blink_LED_PC13();
                     s_move_stage = M_STAGE_D_TO_A;
                     s_start_yaw = g_mpu_data.yaw; // 记录开始转弯的角度
                 }
